8f40ecd84bb3438fd40c4a45932189f3ae56bfe6
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-01 15:31:14 -0300

    arquivo alterado para a raiz do projeto

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <br>
+    <div id="dMsg"></div> <!-- Área para Mensagens de Validação -->
+    <br>
+    <form id="Cadastro" action="salvar_cadastro.php" method="post">
+        <label for="Nome">Nome do motorista:</label>
+        <input id="Nome" name="Nome" type="input" value="">
+        <br>
+        <label for="Carro_modelo">Modelo do veículo:</label>
+        <input id="Carro_modelo" name="Carro_modelo" type="input" value="">
+        <br>
+        <label for="Placa">Placa do veículo:</label>
+        <input id="Placa" name="Placa" type="input" value="">
+        <br>
+        <label for="Local_origem">Local de origem:</label>
+        <select id="Local_origem" name="Local_origem"> <!--Esse será alterado-->
+            <option value="SE">Selecionar</option>
+            <option value="sc">Santa Catarina</option>
+        </select>
+        <br>
+        <label for="sCidade">Cidade:</label>
+        <select id="sCidade" name="sCidade"> <!--Esse será alterado-->
+            <option value="00">Selecionar</option>
+            <option value="1212112">Rio do Sul</option>
+        </select>
+        <br>
+        <label for="Local_destino">Local de origem:</label>
+        <select id="Local_destino" name="Local_destino"> <!--Esse será alterado-->
+            <option value="SE">Selecionar</option>
+            <option value="sc">Santa Catarina</option>
+        </select>
+        <br>
+        <label for="sCidade">Cidade:</label>
+        <select id="sCidade" name="sCidade"> <!--Esse será alterado-->
+            <option value="00">Selecionar</option>
+            <option value="1212112">Rio do Sul</option>
+        </select>
+        <br><br>
+        <input id="bLimpar" type="reset" value="Limpar">&nbsp;|&nbsp;
+        <input id="bSalvar" name="bGravar" type="submit" value="Gravar">
+    </form>
+</body>
+<!-- Chamada Biblioteca JS do JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- Chamada Biblioteca JS da Aplicação -->
+<script language="JavaScript" src="lib/funcoes.js"></script>
+</html>
